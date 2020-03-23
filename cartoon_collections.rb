1b2af14eb06a712240f426d512296c5fc85d92d7
Derek Williams <jogamer130@gmail.com>
2020-03-23 05:26:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  trings.find do |x|
+  strings.find do |x|
     cheese_types.include?(x)
   end
 end
